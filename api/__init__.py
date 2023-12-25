f46efc56df27260bc7eaad77300acac06291941b
Prerit Das <preritdas@gmail.com>
2023-12-25 00:23:08 -0500

    Endpoint to get the latest Candle.

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,10 @@
 """Root API."""
 from fastapi import FastAPI
 
+import random
+
+from api.models import Candle
+
 
 app = FastAPI(
     title="Market Candle API",
@@ -13,3 +17,14 @@ app = FastAPI(
 async def root():
     """Root endpoint."""
     return "API is alive and well."
+
+
+@app.get("/candle")
+async def candle() -> Candle:
+    """Get the latest candle data. Mocked currently."""
+    return Candle(
+        candle_top=random.randint(4, 9),
+        candle_bottom=random.randint(4, 9),
+        wick_top=random.randint(4, 9),
+        wick_bottom=random.randint(4, 9),
+    )
