8e8d0f7ae6d65de42433b11492d36cb01f8d22b1
Prerit Das <preritdas@gmail.com>
2023-12-25 00:22:50 -0500

    Candle model.

--- api/models.py
+++ api/models.py
@@ -1,2 +1,34 @@
 """Models for the API response."""
 from pydantic import BaseModel, Field
+
+
+class Candle(BaseModel):
+    """Candle model."""
+    candle_top: float = Field(
+        ..., 
+        description="Geopoisition of the top of the candle.",
+        example=1.0, 
+        gte=-10.0, 
+        lte=10.0
+    )
+    candle_bottom: float = Field(
+        ..., 
+        description="Geopoisition of the bottom of the candle.",
+        example=1.0, 
+        gte=-10.0, 
+        lte=10.0
+    )
+    wick_top: float = Field(
+        ..., 
+        description="Geopoisition of the top of the wick.",
+        example=1.0, 
+        gte=-10.0, 
+        lte=10.0
+    )
+    wick_bottom: float = Field(
+        ..., 
+        description="Geopoisition of the bottom of the wick.",
+        example=1.0, 
+        gte=-10.0, 
+        lte=10.0
+    )
